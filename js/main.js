77f54a9f2f4b1037821389b905b610b6831dfa4c
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-10 15:58:19 +0100

    added user relation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var words = 3
+var words = 300
 var wordCount = 0
 var timeSet = false
 var interval;
 var minutes = 0;
-var seconds = 3;
+var seconds = 1;
 var sent = false;
 var duration = 0;
 
@@ -11,7 +11,11 @@ $(document).ready(function() {
 	timer = setInterval(function() {
 		duration ++;
 	}, 1000);
-
+	function getURLParameter(name) {
+	    return decodeURI(
+	        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+	    );
+	}
 	function countdown(element) {
 	    interval = setInterval(function () {
 	        var el = document.getElementById(element);
@@ -36,34 +40,51 @@ $(document).ready(function() {
 	        var second_text = seconds > 1 ? 'seconds' : 'second';
 	        el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + ' remaining';
 	        seconds--;
-	
+
 	    }, 1000);
 	}
-	
+
 	function submitEntry() {
 		$(".message").innerHTML = "Submiting your words...";
 		//send to parse
 		var Entries = Parse.Object.extend("Entries");
+		var Users = Parse.Object.extend("MagentaUser");
 		var entry = new Entries;
-		
-		entryText = $("#block").val();
-		entry.set("entryText",entryText);
-		entry.set("duration", duration);
-		entry.set("wordCount", wordCount);
-		
-		entry.save(null, {
-			success: function(entry) {
-				console.log("entry added to db");
-				$("#state").html("Thanks, your entry has been securely saved");
-				$("#block").attr("disabled","disabled");
-				sent = true;
-			},
-			error: function(entry, error) {
+		var userID = getURLParameter("uid")
+		var user = undefined
 
-			}
+		var query = new Parse.Query(Users);
+		query.get(userID, {
+		  success: function(usr) {
+		  	user = usr;
+		  	entryText = $("#block").val();
+		  	entry.set("entryText",entryText);
+		  	entry.set("duration", duration);
+		  	entry.set("wordCount", wordCount);
+		  	entry.set("parent", user);
+		  	entry.save(null, {
+		  		success: function(entry) {
+		  			console.log("entry added to db");
+		  			$("#state").html("Thanks, your entry has been securely saved");
+		  			$("#block").attr("disabled","disabled");
+		  			sent = true;
+		  		},
+		  		error: function(entry, error) {
+
+		  		}
+		  	});
+		  },
+		  error: function(object, error) {
+		    // The object was not retrieved successfully.
+		    // error is a Parse.Error with an error code and description.
+		  }
 		});
+
+
+
+
 	}
-	
+
 	$("#countdown-words").html(words + ' words to go')
 
 	function countWords(s){
@@ -72,17 +93,17 @@ $(document).ready(function() {
 		s = s.replace(/\n /,"\n");
 		return s.split(' ').length;
 	}
-	
+
 	$("#submit_button").click(function() {
 		submitEntry();
 	});
-	
+
 	$("#block").on('input propertychange', function () {
 	    if (timeSet === false) {
 	        timeSet = true;
 	        countdown('countdown-time');
 	    }
-	
+
 	    wordCount = countWords($("#block").val());
 		if (wordCount >= words) {
 			$("#countdown-words").hide();
