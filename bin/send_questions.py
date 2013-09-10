9f5e4c1c178444ba90a4eb4f86dda42efc81bbe8
Michael Detmold <michael.detmold@thatcrowdthing.com>
2013-09-10 10:33:04 +0100

    added todo in python file

---
+++ bin/send_questions.py
@@ -0,0 +1,2 @@
+# use this https://devcenter.heroku.com/articles/scheduler to schedule email sending
+# write a python script that goes throught all of the email entries in parse, and sends an email for each of them.
